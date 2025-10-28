2c42086a4b78925bc15a7733dc0f1e87fec99547
A.Basalov <6y6jlb@gmail.com>
2025-10-28 18:46:35 +0300

    joke widget extracted from hs

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,75 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:pomodoro_sczuw/utils/styles/app_text_styles.dart';
-import 'package:pomodoro_sczuw/providers/random_joke_provider.dart';
-import 'package:pomodoro_sczuw/services/i_10n.dart';
+import 'package:pomodoro_sczuw/widgets/joke_widget.dart';
 
-class HomeScreen extends ConsumerStatefulWidget {
+class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
-  ConsumerState<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends ConsumerState<HomeScreen> {
-  @override
   Widget build(BuildContext context) {
-    final jokeAsync = ref.watch(randomJokeProvider);
-
     return Scaffold(
       appBar: AppBar(title: Text('from HOME PAGE', style: AppTextStyles.title)),
       body: Padding(
         padding: const EdgeInsets.all(20.0),
-        child: Center(
-          child: SizedBox(
-            width: double.infinity,
-            child: jokeAsync.when(
-              data: (joke) => ElevatedButton(
-                onPressed: () {
-                  ref.invalidate(randomJokeProvider);
-                },
-                style: ElevatedButton.styleFrom(
-                  padding: const EdgeInsets.all(20.0),
-                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12.0)),
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(joke.setup, textAlign: TextAlign.center, style: AppTextStyles.caption),
-                    const SizedBox(width: 20),
-                    Text(joke.punchline, textAlign: TextAlign.center, style: AppTextStyles.captionBold),
-                  ],
-                ),
-              ),
-              loading: () => ElevatedButton(
-                onPressed: null,
-                style: ElevatedButton.styleFrom(
-                  padding: const EdgeInsets.all(20.0),
-                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12.0)),
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [const SizedBox(width: 20, height: 20, child: CircularProgressIndicator(strokeWidth: 2))],
-                ),
-              ),
-              error: (error, stack) => ElevatedButton(
-                onPressed: () {
-                  ref.invalidate(randomJokeProvider);
-                },
-                style: ElevatedButton.styleFrom(
-                  padding: const EdgeInsets.all(20.0),
-                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12.0)),
-                  backgroundColor: Colors.red,
-                  foregroundColor: Colors.white,
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [const Icon(Icons.error), const SizedBox(width: 10), Text(I10n().t.joke_error)],
-                ),
-              ),
-            ),
-          ),
-        ),
+        child: Center(child: JokeWidget()),
       ),
     );
   }
